d54d6bed2b84fd0b289065bbb829531c1e0ba48f
AMG0524 <abdul.g@bk.ru>
2024-07-30 11:29:06 +0300

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,27 @@
+calls = 0
+
+def count_calls():
+    global calls
+    calls += 1
+
+def string_info(string):
+    global calls
+    calls += 1
+    str_down = string.lower()
+    str_up = string.upper()
+    str_length = len(string)
+    return str_length, str_up, str_down
+
+def is_contains (string, list_to_search):
+    global calls
+    calls += 1
+    for i in list_to_search:
+        if string.upper() == i.upper():
+            return True
+    return False
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
